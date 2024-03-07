fd714e6d46f8d37fc0dec47bc3eb3694722ccaee
SaRoCS <samuel.schroeder13@gmail.com>
2024-03-07 10:58:09 +0000

    Added documentation

--- src/main/java/com/example/cs4076_clientfx/App.java
+++ src/main/java/com/example/cs4076_clientfx/App.java
@@ -30,15 +30,40 @@ import java.time.LocalTime;
  * JavaFX App
  */
 public class App extends Application {
+    /**
+     * The port to communicate on
+     */
     static final int PORT = 1234;
+    /**
+     * The client's address
+     */
     static InetAddress host;
+    /**
+     * The current state of the connection with the server
+     */
     static boolean connectionOpen = false;
+    /**
+     * The communication socket
+     */
     static Socket link = null;
 
     public static void main(String[] args) {
         launch();
     }
 
+    /**
+     * Sends the given data in JSON format and receives the server's response
+     *
+     * @param in         The socket's input
+     * @param out        The socket's output
+     * @param userAction The desired action to take
+     * @param className  The name of the class to perform the action on
+     * @param classDate  The date of the class to perform the action on
+     * @param startTime  The start time of the class to perform the action on
+     * @param endTime    The end time of the class to perform the action on
+     * @param roomNumber The room number of the class to perform the action on
+     * @return The server's response
+     */
     private static JSONObject sendData(ObjectInputStream in, ObjectOutputStream out, String userAction, String className, String classDate, LocalTime startTime, LocalTime endTime, String roomNumber) {
         JSONObject obj = new JSONObject();
         JSONObject data = null;
@@ -66,6 +91,9 @@ public class App extends Application {
         return res;
     }
 
+    /**
+     * Opens a connection with the server
+     */
     private static void openConnection() {
         try {
             host = InetAddress.getLocalHost();
@@ -82,6 +110,9 @@ public class App extends Application {
         }
     }
 
+    /**
+     * CLoses the connection with the server
+     */
     private static void closeConnection() {
         try {
             System.out.println("\n* Closing connection... *");
@@ -95,6 +126,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        // Create the GUI elements
         TextField classNameTextField = new TextField();
         DatePicker classDatePicker = new DatePicker();
         TextField roomNumberTextField = new TextField();
@@ -110,9 +142,11 @@ public class App extends Application {
         HBox endTimeBox = new HBox(5, new Label("End Time:"), endHours, new Label(":"), endMinutes);
         HBox btnControls = new HBox(25, submitButton, stopButton);
 
+        // Set the necessary properties for the GUI elements
         classNameTextField.setPromptText("Class Name");
         classDatePicker.setPromptText("Class Date");
         roomNumberTextField.setPromptText("Room Number");
+
         actions.getItems().addAll("Add Class", "Remove Class", "Display Schedule");
         serverResponse.setWrapText(true);
         classDatePicker.setEditable(false);
@@ -125,6 +159,7 @@ public class App extends Application {
 
         submitButton.disableProperty().bind(Bindings.isNull(actions.valueProperty()));
 
+        // Disables selecting weekend days in the calendar
         classDatePicker.setDayCellFactory(new Callback<DatePicker, DateCell>() {
             public DateCell call(final DatePicker datePicker) {
                 return new DateCell() {
@@ -140,11 +175,13 @@ public class App extends Application {
             }
         });
 
+        // Format the pane
         GridPane gridPane = new GridPane();
         gridPane.setHgap(8);
         gridPane.setVgap(8);
         gridPane.setPadding(new Insets(5));
 
+        // Add the GUI elements to the pane
         gridPane.addRow(0, classNameTextField, btnControls);
         gridPane.addRow(1, classDatePicker, serverResponse);
         gridPane.addRow(2, startTimeBox);
@@ -152,19 +189,24 @@ public class App extends Application {
         gridPane.addRow(4, roomNumberTextField);
         gridPane.addRow(5, actions);
 
+        // Set the column widths
         ColumnConstraints col1 = new ColumnConstraints();
         ColumnConstraints col2 = new ColumnConstraints();
         col2.setPrefWidth(200);
         gridPane.getColumnConstraints().addAll(col1, col2);
 
+        // Align the server response message
         GridPane.setRowSpan(serverResponse, 5);
         GridPane.setHalignment(serverResponse, HPos.CENTER);
 
         stage.setTitle("Class Scheduler");
+
+        // Removes the focus from any GUI control
         stage.setOnShown(event -> gridPane.requestFocus());
 
         submitButton.setOnAction(event -> {
             try {
+                // Connect to the server if needed
                 if (!connectionOpen) {
                     openConnection();
                 }
@@ -179,16 +221,19 @@ public class App extends Application {
                 LocalTime endTime = null;
                 String roomNumber = null;
 
+                // Collect the form data
                 if (!userAction.equals("Display Schedule")) {
                     className = classNameTextField.getText();
                     classDate = classDatePicker.getValue().getDayOfWeek().name();
-                    startTime = LocalTime.of(startHours.getValue(), startHours.getValue());
-                    endTime = LocalTime.of(endHours.getValue(), endHours.getValue());
+                    startTime = LocalTime.of(startHours.getValue(), Integer.parseInt(startMinutes.getValue()));
+                    endTime = LocalTime.of(endHours.getValue(), Integer.parseInt(endMinutes.getValue()));
                     roomNumber = roomNumberTextField.getText();
                 }
 
+                // Send the form data
                 JSONObject res = sendData(in, out, userAction, className, classDate, startTime, endTime, roomNumber);
 
+                // Display the response
                 if (res != null) {
                     serverResponse.setText(res.get("response").toString());
                 } else {
@@ -201,6 +246,7 @@ public class App extends Application {
 
         actions.setOnAction(event -> {
             String value = actions.getValue();
+            // If the chosen action requires data, disable the submit button until all fields are filled
             if (!value.equals("Display Schedule")) {
                 BooleanBinding requiredFields = Bindings.isEmpty(classNameTextField.textProperty()).or(Bindings.isNull(classDatePicker.valueProperty())).or(Bindings.isNull(startHours.valueProperty())).or(Bindings.isNull(startMinutes.valueProperty())).or(Bindings.isNull(endHours.valueProperty())).or(Bindings.isNull(endMinutes.valueProperty())).or(Bindings.isEmpty(roomNumberTextField.textProperty()));
                 submitButton.disableProperty().bind(requiredFields);
@@ -209,6 +255,7 @@ public class App extends Application {
 
         stopButton.setOnAction(event -> {
             if (connectionOpen) {
+                // Send a stop message to the server
                 ObjectOutputStream out = null;
                 ObjectInputStream in = null;
                 try {
@@ -217,12 +264,15 @@ public class App extends Application {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
                 JSONObject res = sendData(in, out, "STOP", null, null, null, null, null);
+
                 if (res != null) {
                     serverResponse.setText(res.get("response").toString());
                 } else {
                     serverResponse.setText("An error occurred while communicating with the server");
                 }
+
                 closeConnection();
             }
         });
